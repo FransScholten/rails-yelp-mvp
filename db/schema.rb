d0a67db189fda703208d3d3991231ae68229f1cc
Frans Scholten <fjdscholten@gmail.com>
2021-02-17 11:42:07 +0100

    Add restaurant model

--- db/schema.rb
+++ db/schema.rb
@@ -15,9 +15,19 @@ ActiveRecord::Schema.define(version: 2021_02_17_100630) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
-    t.integer "stars"
+    t.string "phone_number"
+    t.string "category"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
+    t.integer "restaurant_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "reviews", "restaurants"
 end

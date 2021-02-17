cb9da9e278bd6ab1a4e08520e29761710373d891
Frans Scholten <fjdscholten@gmail.com>
2021-02-17 16:44:35 +0100

    yelp

--- db/migrate/20210217111241_change_data_type_to_restaurant.rb
+++
@@ -1,7 +0,0 @@
-class ChangeDataTypeToRestaurant < ActiveRecord::Migration[6.0]
-  def change
-    rename_column(:restaurants, :stars, :phone_number)
-    change_column(:restaurants, :phone_number, :string)
-    add_column(:restaurants, :category, :string)
-  end
-end
